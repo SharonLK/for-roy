ca6e374d56701f1c7efef8e52b6169156b59f129
Sharon <sharonkl@campus.technion.ac.il>
2017-05-15 20:58:19 +0300

    yes

---
+++ app/src/main/java/com/askylol/bookaseat/OpeningHoursActivity.java
@@ -0,0 +1,13 @@
+package com.askylol.bookaseat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpeningHoursActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_hours);
+    }
+}
