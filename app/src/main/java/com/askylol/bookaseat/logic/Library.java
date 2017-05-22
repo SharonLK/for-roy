520ef760b0be299ac1c76593447af9d82243e6aa
Sharon <sharonkl@campus.technion.ac.il>
2017-05-22 21:55:34 +0300

    Add basic functions to Library and create Seat class

--- app/src/main/java/com/askylol/bookaseat/logic/Library.java
+++ app/src/main/java/com/askylol/bookaseat/logic/Library.java
@@ -4,5 +4,15 @@ package com.askylol.bookaseat.logic;
  * Created by Sharon on 22-May-17.
  */
 public class Library {
+    public void reserve(int seatId, User user) {
+        // TODO
+    }
 
+    public void free(int seatId, User user) {
+        // TODO
+    }
+
+    public Seat.Status getSeatStatus(int seatId) {
+        return Seat.Status.FREE; // TODO
+    }
 }
---
+++ app/src/main/java/com/askylol/bookaseat/logic/Seat.java
@@ -0,0 +1,12 @@
+package com.askylol.bookaseat.logic;
+
+/**
+ * Created by Sharon on 22-May-17.
+ */
+public class Seat {
+    public enum Status {
+        OCCUPIED,
+        RESERVED,
+        FREE
+    }
+}
