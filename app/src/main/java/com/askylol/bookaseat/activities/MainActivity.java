e305bd8a2e73c87ce6b3983bfa7f519d664210e6
Sharon <sharonkl@campus.technion.ac.il>
2017-05-29 21:31:32 +0300

    yes

--- app/src/main/java/com/askylol/bookaseat/activities/MainActivity.java
+++ app/src/main/java/com/askylol/bookaseat/activities/MainActivity.java
@@ -71,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 library = dataSnapshot.getValue(Library.class);
-                System.out.println(library.getOpeningHours());
                 library.setLibraryRef(libraryRef);
                 tileView.addDetailLevel(1.0f, "tile-%d_%d.jpg", 256, 256);
                 libraryChangedListener.onDataChange(dataSnapshot);
