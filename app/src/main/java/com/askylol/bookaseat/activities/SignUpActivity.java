392a50de23e3445942f6e6e2b9da81b0be1197a4
Sharon <sharonkl@campus.technion.ac.il>
2017-06-10 18:47:42 +0300

    Add activity and layout for Sign Up form

---
+++ app/src/main/java/com/askylol/bookaseat/activities/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.askylol.bookaseat.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.askylol.bookaseat.R;
+
+/**
+ * Created by Sharon on 10-Jun-17.
+ */
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
